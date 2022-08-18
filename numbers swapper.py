5be09ffcf6598b6e28f21f1d0c9cce2d16c0d563
adamossama1 <adamossama357@gmail.com>
2022-08-18 09:19:00 +0200

    python projects

---
+++ numbers swapper.py
@@ -0,0 +1,17 @@
+
+
+
+x=int(input('x='))
+
+y=int(input('y='))
+
+z=int(input('z='))
+
+
+print('before swapping : x=',{x},'y=',{y},'z=',{z},)
+
+
+print('after swapping : x=',{y},'y=',{z},'z=',{x},)
+
+
+print('done')
